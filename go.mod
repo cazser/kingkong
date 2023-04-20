b214c3f33255355db725680b695d5973b2189148
cazser <2264712072@qq.com>
2023-04-20 10:27:20 +0800

    add launch module

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module kingkong
 
 go 1.20
+
+replace kingkong/repl => ./repl
+
+replace kingkong/lexer => ./lexer
+
+replace kingkong/token => ./token
+
+require kingkong/repl v0.0.0-00010101000000-000000000000
+
+require (
+	kingkong/lexer v0.0.0-00010101000000-000000000000 // indirect
+	kingkong/token v0.0.0-00010101000000-000000000000 // indirect
+)
--- repl/repl.go
+++ repl/repl.go
@@ -11,5 +11,20 @@ import (
 const PROMPT =">>"
 
 func Start(in io.Reader, out io.Writer){
-	
+	scanner:= bufio.NewScanner(in);
+
+	for{
+		fmt.Fprintf(out, PROMPT);
+		sacnned:= scanner.Scan();
+		if !sacnned{
+			return;
+		}
+
+		line:= scanner.Text();
+		l:= lexer.New(line);
+
+		for tok:= l.NextToken(); tok.Type!= token.EOF; tok= l.NextToken(){
+			fmt.Fprintf(out, "%+v\n", tok);
+		}
+	}
 }
