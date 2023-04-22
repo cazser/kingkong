81740be002044bb3a8cdef5afd09681041c12485
cazser <2264712072@qq.com>
2023-04-23 07:56:15 +0800

    add precedences to parser

--- parser/parser.go
+++ parser/parser.go
@@ -34,6 +34,18 @@ type(
 	infixParseFn func(ast.Expression) ast.Expression
 )
 
+var precedences = map[token.TokenType]int{
+	token.EQ :  EQUALS,
+	token.NOT_EQ : EQUALS,
+	token.LT : LESSGREATER,
+	token.GT: LESSGREATER,
+	token.PLUS: SUM,
+	token.MINUS : SUM,
+	token.SLASH: PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
+
+
 func New(l *lexer.Lexer) *Parser{
  p:= &Parser{
 	l:l,
@@ -223,4 +235,21 @@ func (p *Parser) parsePrefixExpression() ast.Expression{
 	expression.Right = p.parseExpression(PREFIX);
 
 	return expression;
+}
+
+
+
+func (p *Parser) peekPrecedence() int{
+	if p, ok:= precedences[p.peekToken.Type]; ok{
+		return p;
+	}
+	return LOWEST;
+}
+
+
+func (p *Parser) curPrecedence() int{
+	if p, ok:= precedences[p.curToken.Type]; ok{
+		return p;
+	}
+	return LOWEST;
 }
