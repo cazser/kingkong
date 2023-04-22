b8ad70d03181d7357ee13ef17aef673efbb79929
cazser <2264712072@qq.com>
2023-04-22 08:34:14 +0800

    test pass leave todo

--- parser/parser.go
+++ parser/parser.go
@@ -57,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement{
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement();
+	case token.RETURN:
+		return p.parseReturnStatement();
 	default:
 		return nil;
 	}
@@ -112,3 +114,15 @@ func (p *Parser) peekError(t token.TokenType){
 }
 
 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement{
+	stmt:= &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken();
+
+	//TODO
+	for !p.curTokenIs(token.SEMICOLON){
+		p.nextToken();
+	}
+
+	return stmt;
+}
